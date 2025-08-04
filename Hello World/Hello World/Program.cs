fb096d7e8d31ac0de6d4cbbec2e64e0fb37fbec3
SHWEDUS <krylov1243@gmail.com>
2025-08-04 16:37:45 +0300

    AngryBirds complete

---
+++ Hello World/Hello World/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace StyleErrors
+{
+    class Program
+    {
+        static int A;
+        static int upperBound;
+        
+        static void Main()
+        {
+            upperBound = 100;
+        }
+
+        static string Decode(string str)
+        {
+            return (int.Parse(str.Replace(".", "")) % 1024).ToString();
+        }
+    }
+}
