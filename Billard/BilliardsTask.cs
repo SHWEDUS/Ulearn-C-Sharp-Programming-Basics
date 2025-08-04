eb33ed23a158090b8e57ef2fff99205e71ad23ee
SHWEDUS <krylov1243@gmail.com>
2025-08-04 17:15:11 +0300

    complete Billiards

--- Billard/BilliardsTask.cs
+++ Billard/BilliardsTask.cs
@@ -1,16 +1,17 @@
+using System;
+
 namespace Billiards;
 
 public static class BilliardsTask
 {
     /// <summary>
-    /// 
+    /// Расчёт угла отражения от стены 
     /// </summary>
     /// <param name="directionRadians">Угол направления движения шара</param>
-    /// <param name="wallInclinationRadians">Угол</param>
+    /// <param name="wallInclinationRadians">Угол наклона стены</param>
     /// <returns></returns>
     public static double BounceWall(double directionRadians, double wallInclinationRadians)
     {
-        //TODO
-        return 0.0;
+        return wallInclinationRadians * 2.0 - directionRadians;
     }
 }
