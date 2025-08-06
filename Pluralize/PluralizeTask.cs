4881bf5f6d9071cafd4924441562ef8418c160d3
SHWEDUS <krylov1243@gmail.com>
2025-08-06 20:58:19 +0300

    complete Pluralize

---
+++ Pluralize/PluralizeTask.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Pluralize
+{
+    public static class PluralizeTask
+    {
+        public static string PluralizeRubles(int count)
+        {
+            return (count % 10) switch
+            {
+                1 when count % 100 != 11 => "рубль",
+                >= 2 and <= 4 when (count % 100 < 10 || count % 100 >= 20) => "рубля",
+                _ => "рублей"
+            };
+        }
+    }
+}
